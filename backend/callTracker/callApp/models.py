7dd841bfe035c3c631311682a65e2c4eebf9c63f
gabesharpton <gabriel.sharpton@gmail.com>
2020-03-17 22:41:11 -0500

    added question and choice models

--- backend/callTracker/callApp/models.py
+++ backend/callTracker/callApp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    total_choices = models.IntegerField(default=0)
+    
