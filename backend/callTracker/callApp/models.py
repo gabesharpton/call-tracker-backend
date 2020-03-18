8388a73f0b0318bdaa00a3daadf316d62d5d8917
gabesharpton <gabriel.sharpton@gmail.com>
2020-03-17 23:08:30 -0500

    cleaned up database messages and added first 5 questions

--- backend/callTracker/callApp/models.py
+++ backend/callTracker/callApp/models.py
@@ -7,9 +7,13 @@ class Employee(models.Model):
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    
+    def __str__(self):
+        return self.question_text
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     total_choices = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
     
